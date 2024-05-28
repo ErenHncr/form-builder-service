5b2d53b8339037845aa7a3b7dc3b941edc4a8757
Eren Hançer <erenhncr@gmail.com>
2024-05-28 17:33:24 +0300

    add GetResponseBodyKeys util function

--- util/util.go
+++ util/util.go
@@ -1,7 +1,23 @@
 package util
 
-import "math"
+import (
+	"encoding/json"
+	"io"
+)
 
-func Round2Dec(val float64) float64 {
-	return math.Round(val*100.0) / 100.0
+func GetResponseBodyKeys(body io.ReadCloser) []string {
+	bodyBytes, _ := io.ReadAll(body)
+	jsonMap := make(map[string]json.RawMessage)
+	err := json.Unmarshal(bodyBytes, &jsonMap)
+
+	keys := make([]string, 0)
+	if err != nil {
+		return keys
+	}
+
+	for key := range jsonMap {
+		keys = append(keys, key)
+	}
+
+	return keys
 }
